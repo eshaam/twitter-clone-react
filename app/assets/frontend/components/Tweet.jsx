bb081624fc7a99df4fadb8ebf633fa1f13df4398
Eshaam Rabaney <eshaam.rabaney@gmail.com>
2016-07-17 11:03:24 +0200

    Add Tweet and TweetList component scaffold

---
+++ app/assets/frontend/components/Tweet.jsx
@@ -0,0 +1,13 @@
+export default class Tweet extends React.Component {
+  render() {
+    return (
+      <li className="collection-item avatar">
+        <i className="material-icons circle">person_pin</i>
+        <span className="title">Eshaam Rabaney</span>
+        <p>
+          My #FirstTweet
+        </p>
+      </li>
+    )
+  }
+}
--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -1,10 +1,12 @@
 import TweetBox from "./components/TweetBox"
+import TweetList from "./components/TweetList"
 
 class Main extends React.Component {
   render() {
     return (
       <div class="container">
         <TweetBox />
+        <TweetList />
       </div>
     );
   }
