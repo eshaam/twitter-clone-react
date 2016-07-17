496b91408992307d6449483587d7836a27eded52
Eshaam Rabaney <eshaam.rabaney@gmail.com>
2016-07-17 13:35:17 +0200

    Only mount react app if element is present(when user logged in)

--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -22,10 +22,10 @@ class Main extends React.Component {
 }
 
 let documentReady = () => {
-  ReactDOM.render(
-    <Main />,
-    document.getElementById('react')
-  );
+  let reactNode = document.getElementById('react');
+  if (reactNode) {
+    ReactDOM.render(  <Main />, reactNode);
+  }
 };
 
 $(documentReady);
